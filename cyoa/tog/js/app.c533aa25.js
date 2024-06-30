e5faf515fbbecb02e77763a2aea2e97af5b0fae3
Hannah <59987316+spartango86@users.noreply.github.com>
2024-06-30 03:13:42 -0500

    Add files via upload

--- cyoa/tog/js/app.c533aa25.js
+++ cyoa/tog/js/app.c533aa25.js
@@ -1925,7 +1925,7 @@
                     if (t.reqId == this.app.rows[e].objects[i].id) {
                       return t.beforeText + " " + this.app.rows[e].objects[i].title + " " + t.afterText;
                     } else if(req == this.app.rows[e].objects[i].id) {
-                      return t.beforeText + " " + this.app.rows[e].objects[i].title + " " + t.afterText;
+                      return t.beforeText + " " + this.app.rows[e].objects[i].title + ` x${count} ` + t.afterText;
                     }
               } else if ("points" == t.type) {
               for (e = 0; e < this.app.pointTypes.length; e++)
