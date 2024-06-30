95438a28ebbf7ca2ca1918e471bd154b0ac319af
Hannah <59987316+spartango86@users.noreply.github.com>
2024-06-30 06:38:07 -0500

    Add files via upload

--- cyoa/tog/js/app.c533aa25.js
+++ cyoa/tog/js/app.c533aa25.js
@@ -3731,8 +3731,8 @@
                         if (c != h && 2 == i.requireds[s].operator) return !1;
                         if (c < h && 3 == i.requireds[s].operator) return !1
                       }
-                    }if(i.id == "pdzygn") return !0;
-                    if (!i.requireds[s].required) {
+                    }
+                    if (!i.requireds[s].required) {if(i.id == "pdzygn") return !0;
                       if (t.app.activated.includes(i.requireds[s].reqId) && "id" == i.requireds[s].type) return !1;
                       if ("points" == i.requireds[s].type) {
                         if ("undefined" == typeof i.requireds[s].operator) {
