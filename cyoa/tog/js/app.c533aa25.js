97f289579afd82d3f1c8c6071adc67dbe619cf0e
Hannah <59987316+spartango86@users.noreply.github.com>
2024-07-01 07:18:20 -0500

    Add files via upload

--- cyoa/tog/js/app.c533aa25.js
+++ cyoa/tog/js/app.c533aa25.js
@@ -2257,7 +2257,7 @@
             sanitizeArg: {
               allowedTags: ["address", "article", "aside", "img", "footer", "header", "h1", "h2", "h3", "h4", "h5", "h6", "hgroup", "main", "nav", "section", "blockquote", "dd", "div", "dl", "dt", "figcaption", "figure", "hr", "li", "main", "ol", "p", "pre", "ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data", "dfn", "em", "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp", "small", "span", "strong", "sub", "sup", "time", "u", "var", "wbr", "caption", "col", "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr"],
               allowedAttributes: {
-                a: ["href", "target", "rel"],
+                a: ["href", "target", "rel", "id"],
                 div: ["class", "id"],
                 p: ["style", "class"],
                 b: ["style"],
